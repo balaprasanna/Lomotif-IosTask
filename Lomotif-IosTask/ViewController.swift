79b0b16cfbb05262057223c5c6874dc186c72dd1
balaprasanna <balaprasannav2009@gmail.com>
2015-08-03 22:52:35 +0800

    Initial Commit

---
+++ Lomotif-IosTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lomotif-IosTask
+//
+//  Created by Prasanna V on 03/08/15.
+//  Copyright (c) 2015 swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
