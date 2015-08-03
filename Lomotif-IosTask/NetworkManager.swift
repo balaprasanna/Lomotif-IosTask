f77d869d7a1f7faae2c3910592bf99b65b93852a
balaprasanna <balaprasannav2009@gmail.com>
2015-08-04 01:06:42 +0800

    Added an observer pattern to notify the controller about the network operation, that the local model has been populated succesfuully.. so now the controllers job is to present the UI, with real data

--- Lomotif-IosTask/NetworkManager.swift
+++ Lomotif-IosTask/NetworkManager.swift
@@ -25,14 +25,15 @@ class NetworkManager {
             var err: NSError?
             var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as! NSDictionary
             if err != nil {
-                // If there is an error parsing JSON, print it to the console
                 println("JSON Error \(err!.localizedDescription)")
             }
             
             let json = JSON(jsonResult)
-            //println("found \(json) challenges")
+            println("found \(json) challenges")
            
             //PlaylistModel
+            
+
             self.PopulateModel(json);
             
         })
@@ -41,21 +42,30 @@ class NetworkManager {
     
     func PopulateModel(json:JSON){
         let count: Int? = json["feed"]["entry"].array?.count
+
         if let ct = count {
+            
             for index in 0...ct-1 {
-                if let name = json["feed"]["entry"][index]["im:name"]["label"].string {
+                var model = PlaylistModel()
+                
+                if let title = json["feed"]["entry"][index]["im:name"]["label"].string {
+                    model.title = title;
+                    println(title)
+                }
+                if let imageurl = json["feed"]["entry"][index]["im:image"][2]["label"].string {
+                    model.imageUrl = imageurl
+                    println(imageurl)
+                }
+                if let name = json["feed"]["entry"][index]["im:artist"]["label"].string {
+                    model.name = name;
                     println(name)
                 }
-                if let name = json["feed"]["entry"][index]["im:name"]["label"].string {
-                    println(name)
-                }
-                if let name = json["feed"]["entry"][index]["im:name"]["label"].string {
-                    println(name)
-                }
-                if let name = json["feed"]["entry"][index]["im:name"]["label"].string {
-                    println(name)
-                }
+                
+                SingletonModelDB.sharedInstance.arrayOfPlaylistItems.append(model)
             }
         }
+        // Send Notification about model change to controller
+        NSNotificationCenter.defaultCenter().postNotificationName("JsonPopulatesLocalModelAfterANetworkCall", object: nil)
+
     }
 }
--- Lomotif-IosTask/ViewController.swift
+++ Lomotif-IosTask/ViewController.swift
@@ -17,14 +17,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Add observer
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "DataPopulatedNotification:", name:"JsonPopulatesLocalModelAfterANetworkCall", object: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
         NetworkManager().performNetworkOps();
         print("When are u getting printed");
+        println("Finally data");
+        println("Data populated");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func DataPopulatedNotification(notification: NSNotification){
+        //Take Action on Notification
+        println("Objetcs count \(SingletonModelDB.sharedInstance.arrayOfPlaylistItems.count)")
+    }
+    
+    deinit
+    {
+        // Remove from all notifications being observed
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
 
